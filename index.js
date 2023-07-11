de9c2be1d17afbab65b274720fd0ee5cbe7b3ab5
adslucvantien <ads.lucvantien@gmail.com>
2023-07-11 16:23:12 +0700

    V1

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the required modules
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Send the response
+  res.end('Hello, World!\n');
+});
+
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
